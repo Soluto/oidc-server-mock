85c0dec27e463e26fc27fa6d94da1675259fc835
Vitaliy Litovskiy <vli@scalepoint.com>
2019-08-21 16:21:58 +0300

    Adds support for custom identity resources and claims.
    Uses generic version for JsonConverter

--- src/utils/ClaimJsonConverter.cs
+++ src/utils/ClaimJsonConverter.cs
@@ -5,25 +5,21 @@ using Newtonsoft.Json.Linq;
 
 namespace OpenIdConnectServer.Utils
 {
-    public class ClaimJsonConverter : JsonConverter
+    public class ClaimJsonConverter : JsonConverter<Claim>
     {
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, Claim value, JsonSerializer serializer)
         {
             throw new NotSupportedException();
         }
 
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        public override Claim ReadJson(JsonReader reader, Type objectType, Claim existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
         {
             var jObject = JObject.Load(reader);
             var type = jObject["Type"].Value<string>();
             var val = jObject["Value"].Value<string>();
 
             return new Claim(type, val);
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == typeof(Claim);
         }
 
         public override bool CanRead => true;
