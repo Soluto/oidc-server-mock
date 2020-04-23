40449103bbd170f10e0b67d116aa801edef53f40
Tweek blackbox <tweek@soluto.com>
2020-04-23 20:52:28 +0300

    .Net 3.1 routing

--- src/Startup.cs
+++ src/Startup.cs
@@ -15,6 +15,8 @@ namespace OpenIdConnectServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
             services.AddIdentityServer(options =>
                     {
                         options.Discovery.ShowKeySet = true;
@@ -41,6 +43,10 @@ namespace OpenIdConnectServer
             app.UseIdentityServer();
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
         }
     }
 }
