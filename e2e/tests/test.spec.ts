0a31ad5c0852c0c06b2e9047f84af8964d8d347a
AleF83 <AlexanderFux83@gmail.com>
2020-07-20 00:23:11 +0300

    Eslint for E2E and Nuget Updates

--- e2e/tests/test.spec.ts
+++ e2e/tests/test.spec.ts
@@ -1,32 +1,32 @@
-import axios from "axios";
-import { expect } from "chai";
-import * as querystring from "querystring";
-import { decode } from "jws";
+import * as querystring from 'querystring';
+import axios from 'axios';
+import { expect } from 'chai';
+import { decode } from 'jws';
 
-describe("Test", () => {
-    it("should work", async () => {
-        const { CLIENT_CREDENTIALS_CLIENT_ID, CLIENT_CREDENTIALS_CLIENT_SECRET, API_RESOURCE } = process.env;
-        const params = {
-        client_id: CLIENT_CREDENTIALS_CLIENT_ID,
-        client_secret: CLIENT_CREDENTIALS_CLIENT_SECRET,
-        grant_type: "client_credentials",
-        scope: API_RESOURCE,
+describe('Test', () => {
+  it('should work', async () => {
+    const { CLIENT_CREDENTIALS_CLIENT_ID, CLIENT_CREDENTIALS_CLIENT_SECRET, API_RESOURCE } = process.env;
+    const parameters = {
+      client_id: CLIENT_CREDENTIALS_CLIENT_ID,
+      client_secret: CLIENT_CREDENTIALS_CLIENT_SECRET,
+      grant_type: 'client_credentials',
+      scope: API_RESOURCE,
     };
 
-        const response = await axios.post(process.env.OIDC_TOKEN_URL, querystring.stringify(params));
+    const response = await axios.post(process.env.OIDC_TOKEN_URL, querystring.stringify(parameters));
 
-        // tslint:disable-next-line:no-unused-expression
-        expect(response).to.exist;
-        // tslint:disable-next-line:no-unused-expression
-        expect(response.data.access_token).to.exist;
-        const token = decode(response.data.access_token);
+    // tslint:disable-next-line:no-unused-expression
+    expect(response).to.exist;
+    // tslint:disable-next-line:no-unused-expression
+    expect(response.data.access_token).to.exist;
+    const token = decode(response.data.access_token);
 
-        expect(token.header.typ).to.equal('JWT');
-        expect(token.payload['iss']).to.equal('http://oidc-server-mock');
-        expect(token.payload['aud']).to.equal('user-service');
-        expect(token.payload['scope']).to.deep.equal([ 'user-service-scope' ])
-        expect(token.payload['client_id']).to.equal('e2e-client-id');
-        expect(token.payload['string_claim']).to.equal('string_claim_value');
-        expect(token.payload['json_claim']).to.deep.equal(['value1', 'value2']);
-    });
+    expect(token.header.typ).to.equal('JWT');
+    expect(token.payload['iss']).to.equal('http://oidc-server-mock');
+    expect(token.payload['aud']).to.equal('user-service');
+    expect(token.payload['scope']).to.deep.equal(['user-service-scope']);
+    expect(token.payload['client_id']).to.equal('e2e-client-id');
+    expect(token.payload['string_claim']).to.equal('string_claim_value');
+    expect(token.payload['json_claim']).to.deep.equal(['value1', 'value2']);
+  });
 });
