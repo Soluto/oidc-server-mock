552203ba1794126f1f9b451b3c603b8d1ff7fb72
Tino Hager <tino.hager@nager.at>
2020-10-01 10:56:46 +0200

    Add reverse proxy forward logic

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using IdentityServer4;
+﻿using IdentityServer4;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using OpenIdConnectServer.Helpers;
 
 namespace OpenIdConnectServer
 {
-  public class Startup
+    public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -25,6 +25,13 @@ namespace OpenIdConnectServer
             services.AddSession(options =>
             {
                 options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            });
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
             });
 
             services.AddControllersWithViews();
@@ -53,6 +60,7 @@ namespace OpenIdConnectServer
                                           .AllowAnyMethod()
                                           .AllowAnyHeader());
 
+            app.UseForwardedHeaders();
             app.UseIdentityServer();
             app.UseStaticFiles();
             app.UseRouting();
