f265c212303e66de6e3a57c26b37d58be2552108
Alex <AlexanderFux83@gmail.com>
2021-01-06 21:53:28 +0200

    Fix bug in RedirectUriValidator (#65)

--- src/Validation/RedirectUriValidator.cs
+++ src/Validation/RedirectUriValidator.cs
@@ -3,23 +3,23 @@ using IdentityServer4.Models;
 using IdentityServer4.Validation;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace OpenIdConnectServer.Validation
 {
-  internal class RedirectUriValidator : IRedirectUriValidator
+    internal class RedirectUriValidator : IRedirectUriValidator
     {
+        protected bool Validate(string requestedUri, ICollection<string> allowedUris) =>
+            allowedUris.Any(allowedUri => Regex.Match(requestedUri, Regex.Escape(allowedUri).Replace("\\*", "[a-zA-Z0-9.]+?")).Success);
 
         public Task<bool> IsPostLogoutRedirectUriValidAsync(string requestedUri, Client client)
         {
-            return Task.FromResult<bool>(client.RedirectUris.Any(allowedUri =>
-                Regex.Match(requestedUri, Regex.Escape(allowedUri).Replace("\\*", "[a-zA-Z0-9.]+?")).Success
-            ));
+            return Task.FromResult(Validate(requestedUri, client.PostLogoutRedirectUris));
         }
 
         public Task<bool> IsRedirectUriValidAsync(string requestedUri, Client client)
         {
-            return Task.FromResult<bool>(client.RedirectUris.Any(allowedUri =>
-                Regex.Match(requestedUri, Regex.Escape(allowedUri).Replace("\\*", "[a-zA-Z0-9.]+?")).Success
-            ));        }
+            return Task.FromResult(Validate(requestedUri, client.RedirectUris));
+        }
     }
 }
