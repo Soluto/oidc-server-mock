936265d4d9644e3f4dc91e9405049b18fd3f6bb3
AleF83 <alexanderfux83@gmail.com>
2021-03-09 09:21:36 +0200

    Configure Newtonsoft.Json serialization

--- src/Startup.cs
+++ src/Startup.cs
@@ -4,16 +4,21 @@ using Microsoft.Extensions.DependencyInjection;
 using OpenIdConnectServer.Helpers;
 using OpenIdConnectServer.Services;
 using OpenIdConnectServer.Validation;
+using OpenIdConnectServer.JsonConverters;
+using Newtonsoft.Json.Serialization;
 
 namespace OpenIdConnectServer
 {
-    public class Startup
+  public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddNewtonsoftJson(options => {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                options.SerializerSettings.Converters.Add(new ClaimJsonConverter());
+            });
 
             services.AddIdentityServer(options =>
                     {
