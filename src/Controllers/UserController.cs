ef8ebb8daac4a5c92b8253ba485caddd870e630f
Alex <AlexanderFux83@gmail.com>
2021-03-10 21:58:54 +0200

    AddUser endpoint simplified (#76)
    * Fix github workflows
    
    * AddUser endpoint simplified

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Reflection;
+using System.Security.Claims;
 using IdentityServer4.Test;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -10,31 +11,36 @@ namespace OpenIdConnectServer.Controllers
     public class UserController: Controller
     {
         private readonly TestUserStore _usersStore;
-        private readonly ICollection<TestUser> _users;
         private readonly ILogger Logger;
 
         public UserController(TestUserStore userStore, ILogger<UserController> logger)
         {
             _usersStore = userStore;
             Logger = logger;
-
-            var usersField = _usersStore.GetType().GetField("_users", BindingFlags.NonPublic | BindingFlags.Instance);
-            _users = usersField.GetValue(_usersStore) as List<TestUser>;
         }
 
         [HttpGet("{subjectId}")]
         public IActionResult GetUser([FromRoute]string subjectId)
         {
             var user = _usersStore.FindBySubjectId(subjectId);
-            Logger.LogDebug("GetUser: {subjectId}: {user}", subjectId, user);
+            Logger.LogDebug("User found: {subjectId}", subjectId);
             return Json(user);
         }
 
         [HttpPost]
         public IActionResult AddUser([FromBody]TestUser user)
         {
-            _users.Add(user);
-            Logger.LogDebug("AddUser {user}", user);
+            var claims = new List<Claim>(user.Claims);
+            claims.Add(new Claim(ClaimTypes.Name, user.Username));
+            var newUser =_usersStore.AutoProvisionUser("Alex", user.SubjectId, new List<Claim>(user.Claims));
+            newUser.SubjectId = user.SubjectId;
+            newUser.Username = user.Username;
+            newUser.Password = user.Password;
+            newUser.ProviderName = string.Empty;
+            newUser.ProviderSubjectId = string.Empty;
+
+            Logger.LogDebug("New user added: {user}", user.SubjectId);
+
             return Json(user.SubjectId);
         }
     }
